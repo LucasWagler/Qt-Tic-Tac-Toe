b431783e173cb5f0200c859af2d49a1eba632cba
Coleman386 <Coleman386@yahoo.com>
2020-03-12 10:51:48 -0400

    Revert "snake game fixed"
    This reverts commit 901a819f8031e9cf64151a6178fc8b5c454cbc76.

--- mainwindow.h
+++ mainwindow.h
@@ -1,39 +1,52 @@
+// Lucas Wagler
+// Dr. Khadka
+// 2020-03-11
+// Tic-Tac-Toe with Victory Lines
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QPainter>
-#include <QTimer>
+#include "game.h"
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    virtual void paintEvent(QPaintEvent *event);
-public slots:
-    void myfunction();
-
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_0_clicked();
+
+    void on_pushButton_1_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_8_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_5_clicked();
 
     void on_pushButton_2_clicked();
 
-    void on_lcdNumber_overflow();
+    void on_pushButton_3_clicked();
 
 private:
     Ui::MainWindow *ui;
-    QTimer *timer;
-
-protected:
-    void keyPressEvent(QKeyEvent *e);
+    game gameInstance;
+    void update(int button);
+    void updateButton(int button);
+    void resetBoard();
+    void updateWinLine();
+    void exitDialog();
 };
-
 #endif // MAINWINDOW_H
